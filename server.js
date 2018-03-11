cec5fd5e170dc3163130c92e1da5ecb545b79ecf
Persis Ratouis <persis.ratouis@gmail.com>
2018-03-10 18:05:23 -0800

    formatting reports data from fetch into categories

--- server.js
+++ server.js
@@ -12,9 +12,96 @@ app.use(session({
 	}
 }));
 
-app.get('/', (req,res) => {
-	res.send('Genome Link example');
+var hbs = require('express-handlebars')({
+	defaultLayout: 'main',
+	extname: '.hbs'
 });
+
+app.engine('hbs',hbs);
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+
+const scope = 'report:blood-glucose report:milk-allergy report:peanuts-allergy report:egg-allergy report:carbohydrate-intake report:protein-intake';
+
+app.get('/login', (req, res) => {
+			const authorizeUrl = genomeLink.OAuth.authorizeUrl({ scope: scope });
+			res.render('login', { authorize_url: authorizeUrl});
+});
+
+app.get('/logout', (req, res) => {
+		req.session.destroy((err) => {
+			if(err){
+				res.status(500).send('unable to logout');
+			}else{
+				res.redirect('/login');
+			}
+		})
+});
+
+app.get('/', async(req, res) => {
+	if (!req.session.oauthToken) {
+		res.redirect('/login');
+		return;
+	}
+	let reports = [];
+	const scopes = scope.split(' ');
+	reports = await Promise.all(scopes.map( async (name) => {
+		return await genomeLink.Report.fetch({
+			name: name.replace(/report:/g, ''),
+			population: 'european',
+			token: req.session.oauthToken
+		}).then((response) => {
+			return Object.assign({},
+				{name: response.phenotype.display_name.toUpperCase()},
+				{score: response.summary.score},
+				{description: response.summary.text});
+		}).catch((err) =>
+			res.status(404).send('your report is not found\n'+err)
+		);
+	}));
+
+	let categories = {};
+	// let categories = { 'deficiencies': [], 'excess': [], 'normal':[] , 'allergies':[] };
+	reports.forEach((report) => {
+		if(report.name.includes('ALLERGY')){
+			categories.allergies ? categories.allergies.push(report): categories.allergies = [report];
+		}else if(report.score < 2){
+			categories.deficiencies ? categories.deficiencies.push(report): categories.deficiencies = [report];
+		}else if(report.score === 2){
+			categories.normal ? categories.normal.push(report): categories.normal = [report];
+		}else{
+			categories.excess ? categories.excess.push(report): categories.excess = [report];
+		}
+	})
+	// res.json(categories);
+	res.render('index', { categories: categories });
+});
+
+// app.get('/login', async (req,res) => {
+//   const scope = 'report:blood-glucose report:iron report:calcium report:vitamin-b12 report:vitamin-a report:milk-allergy report:peanuts-allergy report:egg-allergy';
+//   const authorizeUrl = genomeLink.OAuth.authorizeUrl({ scope: scope });
+// 	console.log('help');
+//   // Fetching a protected resource using an OAuth2 token if exists.
+//   let reports = [];
+//   if (req.session.oauthToken) {
+//     const scopes = scope.split(' ');
+//     reports = await Promise.all(scopes.map( async (name) => {
+//       return await genomeLink.Report.fetch({
+//         name: name.replace(/report:/g, ''),
+//         population: 'european',
+//         token: req.session.oauthToken
+//       });
+//     }));
+//   }
+// 	// res.render('login', {
+//   //   authorize_url: authorizeUrl,
+//   //   reports: reports,
+//   // });
+// });
+
+app.get('/insight/:healthIndicator', (req, res) => {
+	
+})
 
 app.get('/callback', async (req, res) => {
   // The user has been redirected back from the provider to your registered
